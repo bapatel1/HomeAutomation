b5be300159afb0acbbaed63954ce9888ffe1f4f2
bhavin_patel <bhavin.patel@ua.edu>
2016-09-21 13:10:29 -0500

    updated code

--- src/routes/garage.ts
+++ src/routes/garage.ts
@@ -4,6 +4,7 @@
 import * as express from "express";
 import * as events from "events";
 import * as fs from "fs";
+import * as path from "path";
 
 const exec = require('child_process').exec;
 
@@ -13,23 +14,6 @@ gpio.setup(7, gpio.DIR_OUT);
 module Route {
 
     export class Garage {
-        // function to encode file data to base64 encoded string
-        public base64_encode(file: string) {
-            // read binary data
-            var bitmap = fs.readFileSync(file);
-            // convert binary data to base64 encoded string
-            return new Buffer(bitmap).toString('base64');
-        }
-
-        // function to create file from base64 encoded string
-        public base64_decode(base64str: string, file: string) {
-            // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-            var bitmap = new Buffer(base64str, 'base64');
-            // write buffer to file
-            fs.writeFileSync(file, bitmap);
-            console.log('******** File created from base64 encoded string ********');
-        }
-
         on(req: express.Request, res: express.Response, next: express.NextFunction) {
             gpio.write(7, true, function(err: Error) {
                 if (err) {
@@ -70,7 +54,7 @@ module Route {
                         "Content-Disposition": "attachment; filename=garage.jpg",
                         "content-type": "image/jpg"
                     });
-                    return res.sendFile("../../garage.jpg", { root: __dirname });
+                    return res.sendFile(path.resolve(__dirname + "../../garage.jpg"));
                     // res.sendFile("../../garage.jpg", { root: __dirname });
                     //process.stderr.write(stderr);
                 }
