af417cf4d4832c33a8ef2829b1a284972dd9f2aa
Bhavin Patel <Bhavin Patel>
2016-09-20 19:55:26 -0500

    added take picture

--- build/routes/garage.js
+++ build/routes/garage.js
@@ -1,24 +1,49 @@
 /// <reference path='../typings/tsd.d.ts' />
 "use strict";
+var exec = require('child_process').exec;
 //rpi-gpio Setup
 var gpio = require("../helpers/rpi-gpio.js");
 gpio.setup(7, gpio.DIR_OUT);
-//johnny-five setup
-var five = require("johnny-five");
-var board = new five.Board();
 var Route;
 (function (Route) {
     var Garage = (function () {
         function Garage() {
         }
         //using rpi-gpio
-        Garage.prototype.on = function (req, res, next) {
+        Garage.prototype.on1 = function (req, res, next) {
             //res.json("{title:'garage', message:'ON: Garage'}");
+            gpio.write(7, true, function (err) {
+                if (err) {
+                    console.log('Error writing to pin while opening.');
+                    return res.json("Error - (Garage-OPEN): ${err}");
+                }
+                else {
+                    console.log('Written to pin. Now closing the pin after 2 sec!');
+                    setTimeout(function () {
+                        console.log('closing the pin now.');
+                        gpio.write(7, false, function (err) {
+                            if (err) {
+                                console.log('Error writing to pin while closing.');
+                                return res.json("Error - (Garage-CLOSE): ${err}");
+                            }
+                            else {
+                                gpio.setup(7, gpio.DIR_OUT);
+                                return res.json("Success:Garage ON closed & finished.");
+                            }
+                        });
+                    }, 1500);
+                }
+            });
+            //return res.json("Success:Garage ON finished.");
+        };
+        Garage.prototype.on = function (req, res, next) {
+            //res.json("{title:'garage', message:'OFF: Garage'}");
             gpio.write(7, true, function (err) {
                 if (err)
                     console.log('Error writing to pin');
                 console.log('Written to pin');
             });
+            return res.json("Success:Garage ON finished.");
         };
         Garage.prototype.off = function (req, res, next) {
             //res.json("{title:'garage', message:'OFF: Garage'}");
@@ -27,28 +52,16 @@ var Route;
                     console.log('Error writing to pin');
                 console.log('Written to pin');
             });
+            return res.json("Success:Garage OFF finished.");
         };
-        //using johnny-five
-        Garage.prototype.on1 = function (req, res, next) {
-            //res.json("{title:'garage', message:'ON: Garage'}");
-            board.on("ready", function () {
-                var relay = new five.Relay(7);
-                console.log(relay.type);
-                relay.on();
-                this.repl.inject({ relay: relay });
+        Garage.prototype.takepicture = function (req, res, next) {
+            var child = exec('fswebcam -r 1280×720 image.jpg', function (error, stdout, stderr) {
+                if (error)
+                    console.log(error);
+                res.json(stdout);
+                process.stderr.write(stderr);
             });
-        };
-        Garage.prototype.off1 = function (req, res, next) {
-            //res.json("{title:'garage', message:'OFF: Garage'}");
-            board.on("ready", function () {
-                var relay = new five.Relay({
-                    type: "NC",
-                    pin: 7
-                });
-                console.log(relay.type);
-                relay.off();
-                this.repl.inject({ relay: relay });
-            });
+            //res.json("{title:'index', message:'ON: Index'}");
         };
         return Garage;
     }());
