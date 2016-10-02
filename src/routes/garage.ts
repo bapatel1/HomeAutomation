44049d22be965d31511e42e715c448b5754731e8
Bhavin Patel <bpatel@caps.ua.edu>
2016-10-01 23:34:18 -0500

    updated

--- src/routes/garage.ts
+++ src/routes/garage.ts
@@ -38,7 +38,7 @@ rfSniffer.on ("data", function ( data:RFData ) {
   console.log(data);
   console.log("Code received: " + data.code + " pulse length : " + data.pulseLength);
 
-  if (data.code === "" + config.get("api.garage.sensor.receivercode")) {
+  if (data.code === config.get("api.garage.sensor.receivercode").toString()) {
     // Send the text message.
      console.log("Code Match Found. Now sending Text");
      client.sendMessage({
