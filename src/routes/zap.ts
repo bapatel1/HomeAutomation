bc7079ed4e4527f0bc89c0dbc8c74ae2ba10a98d
Bhavin Patel <bpatel@caps.ua.edu>
2016-10-04 22:14:40 -0500

    adding zap routes

--- src/routes/zap.ts
+++ src/routes/zap.ts
@@ -12,40 +12,51 @@ const rpio = require("rpio");
 const config = require("config");
 const PIN = config.get("api.garage.pin");
 
-var rpi433    = require("rpi-433"),
+var rpi433 = require("rpi-433"),
     rfEmitter = rpi433.emitter({
-      pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
-      pulseLength: 350            //Send the code with a 350 pulse length
+        pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
+        pulseLength: 350            //Send the code with a 350 pulse length
     });
 
 module Route {
-  export class Zap {
-    button1 (req: express.Request, res: express.Response, next: express.NextFunction) {
-      console.log("Button 1 is pressed.");
-      //res.send (req.params);
-      if ( req.params.val === "1") {
-        res.send ("Switch ON");
-      } else {
-        res.send ("Switch OFF");
-      }
-    }
+    export class Zap {
+        button1(req: express.Request, res: express.Response, next: express.NextFunction) {
+            console.log("Button 1 is pressed.");
+            //res.send (req.params);
+            if (req.params.val === "1") {
+                rfEmitter.sendCode(4199731, function (error: string , stdout: any) {   //Send 1234
+                    if (!error) {
+                       console.log(stdout); //Should display code
+                       res.send("Switch ON");
+                     }
+                });
 
-    button2 (req: express.Request, res: express.Response, next: express.NextFunction) {
-      console.log("Button 2 is pressed.");
-    }
+            } else {
+              rfEmitter.sendCode(4199740, function (error: string , stdout: any) {   //Send 1234
+                  if (!error) {
+                     console.log(stdout); //Should display code
+                     res.send("Switch OFF");
+                   }
+              });
+            }
+        }
 
-    button3 (req: express.Request, res: express.Response, next: express.NextFunction) {
-      console.log("Button 3 is pressed.");
-    }
+        button2(req: express.Request, res: express.Response, next: express.NextFunction) {
+            console.log("Button 2 is pressed.");
+        }
 
-    button4 (req: express.Request, res: express.Response, next: express.NextFunction) {
-      console.log("Button 4 is pressed.");
-    }
+        button3(req: express.Request, res: express.Response, next: express.NextFunction) {
+            console.log("Button 3 is pressed.");
+        }
 
-    button5 (req: express.Request, res: express.Response, next: express.NextFunction) {
-      console.log("Button 5 is pressed.");
+        button4(req: express.Request, res: express.Response, next: express.NextFunction) {
+            console.log("Button 4 is pressed.");
+        }
+
+        button5(req: express.Request, res: express.Response, next: express.NextFunction) {
+            console.log("Button 5 is pressed.");
+        }
     }
-  }
 }
 
 export = Route;
