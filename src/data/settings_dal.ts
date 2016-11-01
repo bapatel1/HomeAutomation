8c1575410f50fff55dcac5c43ccb219b0dd987e8
bhavin_patel <bhavin.patel@ua.edu>
2016-11-01 15:47:35 -0500

    updating DAL

--- src/data/settings_dal.ts
+++ src/data/settings_dal.ts
@@ -2,6 +2,7 @@
 
 import * as mongoose from "mongoose";
 import * as Settings from "../model/settings";
+const Q = require("q");
 
 // class SettingsTable {
 //   key: String;
@@ -9,64 +10,69 @@ import * as Settings from "../model/settings";
 // }
 
 class ReturnObject {
-  info: String;
-  error: any;
-  data: any;
+    info: String;
+    error: any;
+    data: any;
 }
 export class SettingsDAL {
 
     public _settings = new Settings();
     public _return = new ReturnObject();
     getSettingsByKey(key: string) {
-      try {
-        Settings.findOne({key: key}, (err: any, results: any) => {
-            if (err) {
-                this._return.info = "Error during getting all settings by key";
-                this._return.error = err;
-                return this._return;
-            } else {
-                console.log(results);
-                this._return.info =  "Settings by KEY found successfully";
-                this._return.data = results;
-                return this._return;
-            }
-        });
-      } catch (e) {
-        this._return.info = "Exception during find settings by KEY";
-        this._return.error = e.message;
-        return this._return;
-      }
+        const q = Q.defer();
+        try {
+            Settings.findOne({ key: key }, (err: any, results: any) => {
+                if (err) {
+                    this._return.info = "Error during getting all settings by key";
+                    this._return.error = err;
+                    q.reject(this._return);
+                } else {
+                    console.log(results);
+                    this._return.info = "Settings by KEY found successfully";
+                    this._return.data = results;
+                    q.resolve(this._return);
+                }
+            });
+        } catch (e) {
+            this._return.info = "Exception during find settings by KEY";
+            this._return.error = e.message;
+            q.reject(this._return);
+        }
+        return q.promise;
     }
 
 
     getSettings() {
-      try {
-        Settings.find({}, (err: any, results: any) => {
-            if (err) {
-                this._return.info =  "Error during find settings";
-                this._return.error = err;
-                return this._return;
-            } else {
-                console.log(results);
-                this._return.info = "Settings found successfully";
-                this._return.data = results;
-                return this._return;
-            }
-        });
-      } catch (e) {
-        this._return.info = "Exception during find settings";
-        this._return.error = e.message;
-        return this._return;
-      }
+        const q = Q.defer();
+        try {
+            Settings.find({}, (err: any, results: any) => {
+                if (err) {
+                    this._return.info = "Error during find settings";
+                    this._return.error = err;
+                    q.reject(this._return);
+                } else {
+                    console.log(results);
+                    this._return.info = "Settings found successfully";
+                    this._return.data = results;
+                    q.resolve(this._return);
+                }
+            });
+        } catch (e) {
+            this._return.info = "Exception during find settings";
+            this._return.error = e.message;
+            q.reject(this._return);
+        }
+        return q.promise;
     }
 
     overrideSettings(key: string, newvalue: any) {
+        const q = Q.defer();
         try {
             Settings.findOne({ key: key }, (err: any, setting: any) => {
                 if (err) {
                     this._return.info = "Error during find settings by key to override";
                     this._return.error = err;
-                    return this._return;
+                    q.reject(this._return);
                 } else {
                     console.log("Found record to Override");
                     setting.value = newvalue;
@@ -74,13 +80,14 @@ export class SettingsDAL {
                     setting.save();
                     this._return.info = "Successfully override settings";
                     this._return.data = setting;
-                    return this._return;
+                    q.resolve(this._return);
                 }
             });
         } catch (e) {
-          this._return.info = "Exception during find override settings by KEY";
-          this._return.error = e.message;
-          return this._return;
+            this._return.info = "Exception during find override settings by KEY";
+            this._return.error = e.message;
+            q.reject(this._return);
         }
+        return q.promise;
     }
 }
